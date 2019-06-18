cda2405de4c276bf810902ab0fa5ca8de645f7a6
yogsothoth1984 <42740581+yogsothoth1984@users.noreply.github.com>
2019-06-18 20:46:18 -0300

    Subida Inicial

---
+++ GestionDeExcepciones/src/gestiondeexcepciones/GestionDeExcepciones.java
@@ -0,0 +1,10 @@
+package gestiondeexcepciones;
+
+public class GestionDeExcepciones {
+
+    public static void main(String[] args) {
+        Ventana vent=new Ventana();
+        vent.setVisible(true);        
+    }
+    
+}
